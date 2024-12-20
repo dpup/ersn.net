2d29660c8ba6813198411cee3ee0e935e478333f
Daniel Pupius <dan@pupi.us>
2024-12-19 21:19:33 -0800

    New logo, styling tweaks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,19 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        serif: ['Merriweather', 'serif'],
         sans: ['Inter', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+      },
+      fontSize: {
+        // Custom size scale
+        xs: ['0.75rem', { lineHeight: '1rem' }],
+        sm: ['0.875rem', { lineHeight: '1.25rem' }],
+        base: ['1rem', { lineHeight: '1.5rem' }],
+        lg: ['1.125rem', { lineHeight: '1.75rem' }],
+        xl: ['1.25rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.5rem', { lineHeight: '2rem' }],
+        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+        '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
       },
       // Styles for 'prose' classes.
       typography: ({ theme }) => ({
